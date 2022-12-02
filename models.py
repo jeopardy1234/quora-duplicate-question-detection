78b08a7eb97c3b96bb4072f0ec19c25de2f9ddd9
Jeopardy <jeopardy@pop-os.localdomain>
2022-12-02 19:00:08 +0530

    LSTM and BiLSTM bugs fixed. Accuracy improved

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-from tensorflow.python.keras.layers import *
-from tensorflow.python.keras.models import Model
+from keras.layers import *
+from keras.models import Model
 import tensorflow as tf
 import numpy as np 
 from keras import Sequential
+from keras.optimizers import adam_v2
 
 class NeuralModels:
     def __init__(self, emb_mat, vocab_size = -1, loss = "binary_crossentropy", epochs = 10, optimizer = "adam", metrics = ["accuracy"]):
@@ -35,20 +36,20 @@ class CBOW(NeuralModels):
     def predict(self, xtest):
         return self.model.predict(xtest)
 
-class LSTM(NeuralModels):
+class LsTM(NeuralModels):
     def __init__(self, emb_mat, vocab_size = -1, loss = "binary_crossentropy", epochs = 10, optimizer = "adam", metrics = ["accuracy"]):
         super().__init__(emb_mat, vocab_size, loss, epochs, optimizer, metrics)
     
     def train_model(self):
-        inp1 = Input(shape = (30,))
-        inp2 = Input(shape = (30,))
-        emb1 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=30)(inp1)
-        emb2 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=30)(inp2)
+        inp1 = Input(shape = (128,))
+        inp2 = Input(shape = (128,))
+        emb1 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=128)(inp1)
+        emb2 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=128)(inp2)
         concat = Concatenate(axis = -1)([emb1 + emb2, emb1 - emb2, emb1 * emb2])
-        lstm = tf.keras.layers.LSTM(150, return_sequences=False, dropout=0.1, return_state=True, kernel_regularizer=tf.keras.regularizers.l2(0.01))(concat)
+        lstm = LSTM(150, return_sequences=False, dropout=0.2, return_state=True)(concat)
         out = Dense(2, activation = "softmax")(lstm[2])
         self.model = Model(inputs = [inp1, inp2], outputs = out)
-        self.model.compile(loss = self.loss, optimizer = self.optimizer, metrics = self.metrics)
+        self.model.compile(loss = self.loss, optimizer = adam_v2.Adam(learning_rate=0.0008), metrics = self.metrics)
 
     def get_model_summary(self):
         self.model.summary()
@@ -61,14 +62,15 @@ class BiLSTM(NeuralModels):
         super().__init__(emb_mat, vocab_size, loss, epochs, optimizer, metrics)
     
     def train_model(self):
-        inp1 = Input(shape = (30,))
-        inp2 = Input(shape = (30,))
-        emb1 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=30)(inp1)
-        emb2 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=30)(inp2)
+        inp1 = Input(shape = (128,))
+        inp2 = Input(shape = (128,))
+        emb1 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=128)(inp1)
+        emb2 = Embedding(output_dim=300, weights = [self.embedding_matrix], trainable = False, input_dim=self.vocab_size, input_length=128)(inp2)
         concat = Concatenate(axis = -1)([emb1 + emb2, emb1 - emb2, emb1 * emb2])
-        bilstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150, return_sequences=False, dropout=0.1, return_state=True, kernel_regularizer=tf.keras.regularizers.l2(0.01)))(concat)
-        out = Dense(2, activation = "softmax")(bilstm)
-        self.model = Model(inputs = [inp1, inp2], outputs = out)
+        out = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150, kernel_regularizer='l2', dropout=0.1, return_sequences=True))(concat)
+        out = tf.keras.backend.mean(out, axis=1, keepdims=False)
+        output = tf.keras.layers.Dense(2, kernel_regularizer='l2', activation='softmax')(out)
+        self.model = Model(inputs = [inp1, inp2], outputs = output)
         self.model.compile(loss = self.loss, optimizer = self.optimizer, metrics = self.metrics)
 
     def get_model_summary(self):
